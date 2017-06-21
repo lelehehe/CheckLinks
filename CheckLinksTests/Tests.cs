bf124989a0b90917cb73809abb5a23b4fd01e3f1
Mike Zhang <mzseattle@gmail.com>
2017-06-21 13:20:14 -0700

    dotnet core learning code init
    first checkin

---
+++ CheckLinksTests/Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CheckLinksTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
